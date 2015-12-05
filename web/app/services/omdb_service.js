b18883cd4be2bd120a543574495bc9d8e79f0ed3
virtalas <virtalas@users.noreply.github.com>
2015-12-05 13:46:11 +0200

    omdb fixing

--- web/app/services/omdb_service.js
+++ web/app/services/omdb_service.js
@@ -1,5 +1,5 @@
 MovieApp.service('OMDbService', function ($http) {
     this.findMovies = function (title, year) {
-        return $http.get('http://www.omdbapi.com/', {params: {s: title, y: year}});
+        return $http.get('//www.omdbapi.com/', {params: {s: title, y: year}});
     }
 });
